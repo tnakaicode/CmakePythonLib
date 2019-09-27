6c74c8f542281833ec59150ae31cb006c456a9a7
Taku Nakai <t.nakai.office@gmail.com>
2019-09-27 19:57:24 +0900

    setup

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup, Extension
 if __name__ == '__main__':
     setup(name='foo',
           version='${PACKAGE_VERSION}',
-          package_dir={ '': '${CMAKE_CURRENT_SOURCE_DIR}' },
+          package_dir={'': '${CMAKE_CURRENT_SOURCE_DIR}'},
           packages=['module'])
